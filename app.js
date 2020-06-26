cc30972a47dccb280390c7f0f334a5c021c8946d
Nicolas <cathyannick@hotmail.fr>
2020-06-26 14:58:46 +0200

    frist

---
+++ app.js
@@ -0,0 +1,52 @@
+var userChoise = prompt("choisisez entre feuille ciseau pierre")
+console.log(userChoise);
+
+var computerChoise = Math.random()
+console.log(computerChoise);
+if (computerChoise < 0.33 ){
+    computerChoise = "feuille"
+}else if (computerChoise <= 0.67){
+    computerChoise= "ciseau"
+}else{
+    computerChoise="pierre"
+}
+console.log(computerChoise);
+
+var compare = function(choice1, choice2){
+    if(choice1 === choice2){
+        return "equality"
+    }
+
+
+    else if(choice1 === "pierre"){
+        if (choice2 === "ciseau"){
+            return "caillou gagne"
+        }
+        else if (choice2 === "feuille"){
+            return "feuille à gagner"
+        }
+    }
+
+
+    else if(choice1 === "feuille"){
+        if (choice2 === "ciseau"){
+            return "ciseau gagne"
+        }
+        else if (choice2 === "pierre"){
+            return "feuille à gagner"
+        }
+    }
+
+
+    else if(choice1 === "ciseau"){
+        if (choice2 === "feuille"){
+            return "ciseau gagne"
+        }
+        else if (choice2 === "pierre"){
+            return "pierre à gagner"
+        }
+    }
+    
+}
+
+console.log("resulta : " , compare(userChoise,computerChoise));
